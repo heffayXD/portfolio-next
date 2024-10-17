7b722270e82c12a9c6e89d899c561b1cdb2ab447
Jeff <jeff@beargroup.com>
2024-10-16 19:04:51 -0700

    Created about section

---
+++ src/components/About/index.tsx
@@ -0,0 +1,22 @@
+const About = () => {
+  return (
+    <div
+      id='about'
+      className='bg-white text-primary py-12'
+    >
+      <h3 className='text-4xl text-center w-full font-medium mb-4'>About</h3>
+      <p className='p-4 max-w-[800px] mx-auto text-justify'>
+        Hey. I&apos;m Jeff - a Full Stack Developer with around 6 years of experience and a passion for working on projects that will 
+        teach me something new. I&apos;ve worked on building customer facing E-Commerce sites alongside teams of developers, designing 
+        backend systems and workflows for small businesses, launched Google Play and Windows applications, created custom 
+        algorithms for determining monthly subscription products, led large database migrations, and more. I don&apos;t like to hold 
+        myself down to one tech stack, and will learn what is necessary to get a project up and running.
+        <br />
+        <br />
+        If you have an idea for something unique, I&apos;d be more than happy to help.
+      </p>
+    </div>
+  )
+}
+
+export default About
