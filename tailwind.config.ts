e5803f3527a4a79e9a767ff22c345d93d2ab0bba
Jeff <jeff@beargroup.com>
2024-10-16 18:44:02 -0700

    Created custom tailwind values

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,36 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
+      animation: {
+        'scroll': 'scroll 16s linear infinite'
+      },
+      keyframes: {
+        scroll: {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(calc(-50% - 4rem))' },
+        }
+      },
+      fontFamily: {
+        sans: ['var(--font-roboto)', 'sans-serif'],
+        roboto: ['var(--font-roboto)', 'sans-serif'],
+        montsrrat: ['var(--font-montserrat)', 'sans-serif']
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+        primary: '#121212',
+        error: 'rgb(255, 0, 0)'
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
+
+export default config
