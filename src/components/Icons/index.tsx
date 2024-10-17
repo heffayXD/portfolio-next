b96b7112af0a9a151da284649c33b15b3a114c29
Jeff <jeff@beargroup.com>
2024-10-16 18:47:18 -0700

    Created Technologies component and Icon sub component

---
+++ src/components/Icons/index.tsx
@@ -0,0 +1,41 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faNode, faPhp, faJsSquare, faLaravel, faSass, faReact, faVuejs, faAws, faPython, faAndroid, faNpm } from '@fortawesome/free-brands-svg-icons'
+
+const Icons = () => {
+  const icons = [
+    { icon: (<FontAwesomeIcon icon={faNode} />), label: 'faNode' },
+    { icon: (<FontAwesomeIcon icon={faPhp} />), label: 'faPhp' },
+    { icon: (<FontAwesomeIcon icon={faJsSquare} />), label: 'faJsSquare' },
+    { icon: (<FontAwesomeIcon icon={faLaravel} />), label: 'faLaravel' },
+    { icon: (<FontAwesomeIcon icon={faSass} />), label: 'faSass' },
+    { icon: (<FontAwesomeIcon icon={faReact} />), label: 'faReact' },
+    { icon: (<FontAwesomeIcon icon={faVuejs} />), label: 'faVuejs' },
+    { icon: (<FontAwesomeIcon icon={faPython} />), label: 'faPython' },
+    { icon: (<FontAwesomeIcon icon={faAws} />), label: 'faAws' },
+    { icon: (<FontAwesomeIcon icon={faAndroid} />), label: 'faAndroid' },
+    { icon: (<FontAwesomeIcon icon={faNpm} />), label: 'faNpm' }
+  ]
+
+  return (
+    <div className='absolute top-8 min-w-[200%] z-10 flex justify-between gap-4 animate-scroll'>
+      {icons.map(({ icon, label }) => (
+        <span
+          className='w-12 h-12 block opacity-10'
+          key={`icon-${label}-1`}
+        >
+          {icon}
+        </span>
+      ))}
+      {icons.map(({ icon, label }) => (
+        <span
+          className='w-12 h-12 block opacity-10'
+          key={`icon-${label}-2`}
+        >
+          {icon}
+        </span>
+      ))}
+    </div>
+  )
+}
+
+export default Icons
---
+++ src/components/Technologies/index.tsx
@@ -0,0 +1,66 @@
+import Icons from '@/components/Icons'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCheck } from '@fortawesome/free-solid-svg-icons'
+
+const experienceArr = [
+  {
+    title: 'Languages',
+    list: ['JavaScript', 'TypeScript', 'PHP', 'Python']
+  },
+  {
+    title: 'Frontend',
+    list: ['React', 'Next.js', 'Sass', 'Tailwind CSS', 'Vue', 'Nuxt.js', 'Hydrogen']
+  },
+  {
+    title: 'Backend',
+    list: ['Node.js', 'Express', 'Laravel', 'GraphQL', 'SQL & NoSQL', 'Serverless']
+  },
+  {
+    title: 'Services',
+    list: ['Shopify', 'AWS', 'Vercel', 'Nacelle', 'Square', 'Sanity', 'PACK Digital']
+  }
+]
+
+const Technologies = () => {
+  return (
+    <div
+      id='experience'
+      className='relative overflow-hidden py-8'
+    >
+      <h1 className='text-4xl text-center w-full font-medium mb-16'>
+        Technologies
+      </h1>
+      <Icons />
+      <div className='flex justify-around flex-wrap w-full px-10 mb-16'>
+        {experienceArr.map(({ title, list }) => (
+          <div
+            className='m-3 mt-6 p-3 flex justify-center flex-wrap'
+            key={`experience-${title}`}
+          >
+            <h3 className='w-full text-center mb-3 text-3xl font-medium'>
+              {title}
+            </h3>
+            <ul>
+              {list.map(item => (
+                <li
+                  className='text-xl'
+                  key={`experience-item-${title}-${item}`}
+                >
+                  <span className='w-4 h-4 inline-block mr-2'>
+                    <FontAwesomeIcon icon={faCheck} />
+                  </span>{item}
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div>
+      <p className='text-center'>
+        ...and more
+      </p>
+    </div>
+  )
+}
+
+export default Technologies
