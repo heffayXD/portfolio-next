37ba36e55a8866d4a4c2ad784e10ebe3d58b0199
Jeff <jeff@beargroup.com>
2024-10-17 20:55:53 -0700

    Created placeholder Experience section

---
+++ src/components/Experience/index.tsx
@@ -0,0 +1,14 @@
+const Experience = () => {
+  return (
+    <div
+      id='experience'
+      className='bg-white text-primary py-12'
+    >
+      <h3 className='text-4xl text-center w-full font-medium mb-4'>
+        Experience
+      </h3>
+    </div>
+  )
+}
+
+export default Experience
