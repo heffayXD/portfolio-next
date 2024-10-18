c2c8f474b4580a4fc3455429cedcd752bed181a6
Jeff <jeff@beargroup.com>
2024-10-17 20:55:45 -0700

    Created placeholder Projects section

---
+++ src/components/Projects/index.tsx
@@ -0,0 +1,11 @@
+const Projects = () => {
+  return (
+    <div id='projects' className='bg-primary py-12'>
+      <h1 className='text-4xl text-center w-full font-medium mb-16'>
+        Past Projects
+      </h1>
+    </div>
+  )
+}
+
+export default Projects
