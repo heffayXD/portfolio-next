860904b7f1bc22d73383d69406fdb31cc596b198
Jeff <jeff@beargroup.com>
2024-10-23 21:15:53 -0700

    Added footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Technologies from '@/components/Technologies'
 import Experience from '@/components/Experience'
 import Projects from '@/components/Projects'
 import Contact from '@/components/Contact'
+import Footer from '@/components/Footer'
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
       <Experience />
       <Contact />
       <Projects />
+      <Footer />
     </main>
   )
 }
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className='bg-primary text-white py-24'>
+      <p className='w-full text-center p-2'>
+        heffay.dev {new Date().getFullYear()}
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
